2cb428df7f87c0aadd03c02826fa343ec28712a0
rida <elbahtouri0123@gmail.com>
2021-01-26 15:19:02 +0100

    crearte customer class

---
+++ src/com/company/Customer.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Customer {
+    private String name;
+    private double Cash;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name =name;
+    }
+    public double getCash() {
+        return Cash;
+    }
+    public void setCash(double cash){
+        this.Cash =cash;
+    }
+
+
+}
