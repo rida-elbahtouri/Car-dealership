5430885ef2e8e095f982d5f9b77c6a6873322da6
rida <elbahtouri0123@gmail.com>
2021-01-27 21:46:34 +0100

    creatre customer from terminal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Customer rida = new Customer();
-        rida.setName("rida");
-        rida.setCash(17000.5);
+        Customer newcust = new Customer();
+        Scanner customer = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter your name");
 
-        Car tesla = new Car();
-        tesla.setModelName("tesla S");
-        tesla.setPrice(20000);
-        rida.testCar(tesla);
-        rida.bycar(tesla);
+        newcust.setName(customer.nextLine());  // Read user input
+        System.out.println("You name is: " + newcust.getName());  // Output user input
+
+        System.out.println("Enter your Cash");
+
+        newcust.setCash(customer.nextDouble());
+        System.out.println("your cash is: " + newcust.getCash() + "$");
+
+
+        //rida.setName("rida");
+        //rida.setCash(17000.5);
+
+        //Car tesla = new Car();
+        //tesla.setModelName("tesla S");
+        //tesla.setPrice(20000);
+        //rida.testCar(tesla);
+        //rida.bycar(tesla);
     }
 }
